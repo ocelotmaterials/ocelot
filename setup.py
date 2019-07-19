3c91455e85bfc7b91ebbdec42cc411136d06cc3a
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2019-07-19 09:01:49 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# GLACIES (Aqua-solids)
+
+import setuptools
+import inspect
+import sys
+import os
+
+long_description = """
+Glacies is a extensible, modular, open-source library for quantum simulation of solids based on planewave basis.
+"""
+
+requirements = [
+
+]

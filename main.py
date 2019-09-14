d9ec37542e3cc08594f6bdbf3816fb80e9fb0779
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2019-09-14 05:05:57 -0300

    Update LICENSE terms

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# This code is part of Ocelot.
+#
+# Copyright (c) 2019 Leandro Seixas Rocha.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
   
 '''
     ocelot - A quantum program to simulate solids in a quantum computer.
@@ -12,8 +28,8 @@ import pandas as pd
 
 from ocelot.heart import *   # core classes and functions
 
-# import ocelot.brain     # basic algorithms
-# import ocelot.eyes      # data analysis and visualization tools
-# import ocelot.claws     # interface with qiskit (for IBMQ), cirq (for Google Quantum AI), and pyquil (for Rigetti Computing)
-# import ocelot.tusks     # optimization algorithms
+# from ocelot.brain import *    # basic algorithms
+# from ocelot.eyes  import *    # data analysis and visualization tools
+# from ocelot.claws import *    # interface with qiskit, cirq, and pyquil
+# from ocelot.tusks import *    # optimization algorithms
 
